60abcef1df640827e546722d3590fff748ab89db
hael-mou <hamzaelmoudden2@gmail.com>
2024-11-17 17:56:49 +0100

    base and chat apps not completed

--- srcs/frontend/app.module.js
+++ srcs/frontend/app.module.js
@@ -5,9 +5,14 @@ import { Router } from "./core/routing.js";
 import { AuthApp } from "./components/authentication/base_page.js";
 import { SignUp } from "./components/authentication/sign_up/app.sign_up.js";
 
+import { BaseApp } from "./components/website/base_page.js";
+import { ChatApp } from "./components/website/chat/chat_app.js";
+
 /* === Custom Elements : ==================================================== */
 customElements.define('auth-app', AuthApp);
 customElements.define('sign-up', SignUp);
+customElements.define('base-app', BaseApp);
+customElements.define('chat-app', ChatApp);
 
 
 /* === DOMContentLoaded : =================================================== */
