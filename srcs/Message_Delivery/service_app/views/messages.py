5602a1ab12742dfe0753bbd195543f5c28e69088
Nassima Dahib <ndahib@c2r11p3.1337.ma>
2024-11-23 18:05:30 +0100

    Adding List Api for chat and Message

---
+++ srcs/Message_Delivery/service_app/views/messages.py
@@ -0,0 +1,33 @@
+
+from rest_framework import generics
+from ..components.Message import Message, MessageSerializer
+
+class MessageView(generics.ListAPIView):
+    permission_classes = []
+    authentication_classes = []
+    serializer_class = MessageSerializer
+    room_id = 0
+    def get_queryset(self):
+        self.room_id = self.request.query_params.get('room')
+        return Message.objects.filter(chat_id=self.room_id).order_by('-sent_at')
+    
+    def filter_queryset(self, queryset):
+        filters = {}
+        query_params = self.request.query_params
+        for key, value in query_params.items():
+            print("key", key, "value", value)
+            if hasattr(Message, key):
+                filters[key] = value
+            elif key == 'start_id':
+                filters['id__gt'] = value
+        queryset = queryset.filter(**filters)
+        return queryset
+
+class UnreadMessage(MessageView):
+
+    pagination_class = None
+    def get_queryset(self):
+        self.room_id = self.request.query_params.get('room')
+        return Message.objects.filter(chat__id=self.room_id).exclude(
+                        status=Message.Status.READ).order_by('-sent_at')
+        
